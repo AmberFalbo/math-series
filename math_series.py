c4a3f8b71d512f34b36830976fa722fffea9f179
Amber Falbo <amberannfalbo@gmail.com>
2020-12-10 22:54:52 -0800

    initial set up

---
+++ math_series.py
@@ -0,0 +1,24 @@
+def fibonacci(n):
+  """[summary]
+
+  Args:
+      n ([type]): [description]
+  """
+
+
+
+def lucas(n):
+  """[summary]
+
+  Args:
+      n ([type]): [description]
+  """
+
+
+def sum_series(n, n):
+  """[summary]
+
+  Args:
+      n ([type]): [description]
+      n ([type]): [description]
+  """
