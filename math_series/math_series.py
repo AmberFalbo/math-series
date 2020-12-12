64da093a6826bae76bf39f69d2d2db20581014f0
Amber Falbo <amberannfalbo@gmail.com>
2020-12-11 20:08:12 -0800

    testing push

--- math_series/math_series.py
+++ math_series/math_series.py
@@ -1,24 +1,32 @@
 def fibonacci(n):
-  """[summary]
+    """This is the fibonacci function
 
-  Args:
-      n ([type]): [description]
-  """
-  
+    Args:
+        n ([type]): [description]
+    """
+    if n < 0:
+      print("Error, try a new number.")
+    
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def lucas(n):
-  """[summary]
+    """This is the lucas function
 
-  Args:
-      n ([type]): [description]
-  """
+    Args:
+        n ([type]): [description]
+    """
 
 
 def sum_series(n, n):
-  """[summary]
+    """This is the sum series function
 
-  Args:
-      n ([type]): [description]
-      n ([type]): [description]
-  """
+    Args:
+        n ([type]): [description]
+        n ([type]): [description]
+    """
